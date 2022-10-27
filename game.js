692fe82430141c16f87f967417589bf7eec44aa1
neoscar10 <77369591+neoscar10@users.noreply.github.com>
2022-10-27 13:17:24 -0700

    initial files

---
+++ game.js
@@ -0,0 +1,82 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickPattern = [];
+var level = 0;
+var started = false;
+
+
+$(".btn").click(function() {
+  var userChosenColor = this.id;
+  userClickPattern.push(userChosenColor);
+  playSound(userChosenColor)
+
+  function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed")
+    setTimeout(function() {
+      $("#" + currentColour).removeClass("pressed")
+    }, 100)
+  }
+  animatePress(userChosenColor)
+
+  checkAnswer(userClickPattern.length - 1)
+});
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickPattern[currentLevel]) {
+    if (userClickPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence()
+      }, 1000);
+      userClickPattern = [];
+    }
+  }else{
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over")
+    },200);
+    $("h1").text("Game Over, Press Any Key To Restart")
+    startOver();
+  }
+};
+
+// Restart Game
+function startOver(){
+  level = 0;
+  userClickPattern = []
+  gamePattern = [];
+  started = false;
+};
+
+$(document).keypress(function(){
+  if (!started){
+    nextSequence();
+    started = true;
+    $("h1").text("Level "+ level)
+  }
+});
+$(document).click(function(){
+  if (!started){
+    nextSequence();
+    started = true;
+    $("h1").text("Level "+ level)
+  }
+});
+
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+};
+
+// function to select a random number between 0-3
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  randomChosenColour = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+  // increasing the level anytime this function is called
+  level++;
+  $("h1").text("Level " + level)
+};
